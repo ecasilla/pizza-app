8be595030bd0340e3971b32be6916a9fbbfe57e7
Ernie Casilla <ecasilla@icloud.com>
2015-01-10 11:10:46 -0300

    added browserify task and working on the server

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,36 @@
-var gulp = require('gulp');
+var gulp       = require('gulp'),
+    browserify = require('browserify'),
+    source     = require('vinyl-source-stream'),
+    sass       = require('gulp-sass');
 
 gulp.task('hello',function() {
   console.log("Hello Gulp!");
 })
 
-gulp.task('default',['hello']);
+gulp.task('scripts',function() {
+ return browserify('./src/scripts/main.js')
+ .bundle()
+ .pipe(source('bundle.js'))
+ .pipe(gulp.dest('dist/scripts'))
+})
+
+gulp.task('html',function() {
+ return gulp
+  .src('src/html/**/*.html')
+  .pipe(gulp.dest('dist'))
+})
+
+gulp.task('images',function() {
+ return gulp
+ .src('src/images/**/*.png')
+ .pipe(gulp.dest('dist/images'))
+})
+
+gulp.task('css',function() {
+ return gulp
+ .src('src/styles/**/*.scss')
+ .pipe(sass())
+ .pipe(gulp.dest('dist/styles'))
+})
+
+gulp.task('default',['css','html','images','hello']);
